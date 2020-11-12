7f1478cc82d022035355818cbd15172e3c688304
SKYLAR DEITZ <sd1081@students.necsd.net>
2020-11-12 15:12:15 +0000

    Finished.

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,17 @@
-
-
+import java.util.Scanner;
 public class Chapter2Test {
   public static void main(String[] args) {
+
+    Scanner scan = new Scanner (System.in);
+
+    System.out.println("Give integer pls");
+    int x = scan.nextInt();
+    System.out.println("Ok one more");
+    int y = scan.nextInt();
   
+    double answer = (x + y) / 2.0;
+
+    System.out.println("Ok your average is: " + answer);
+
   }
 }
